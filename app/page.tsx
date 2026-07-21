975ebf6a2d84b4916d61708836c101b97eb98934
Latnove <r1ghtnowcp2@gmail.com>
2026-07-21 18:48:27 +0300

    feat: add user info in chat list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 import { HomePage } from '@/_pages/home'
 import { LogoutButton } from '@/features/logout/index.server'
+import { isAuthUser } from '@/shared/lib'
 import { auth } from '@/shared/lib/index.server'
 import { redirect } from 'next/navigation'
 
 export default async function HomeRoute() {
   const session = await auth()
 
-  if (!session?.user) {
+  if (!isAuthUser(session?.user)) {
     redirect('/login')
   }
 
-  return <HomePage actions={<LogoutButton iconOnly />} />
+  return (
+    <HomePage
+      authUser={{
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+      }}
+
+      actions={<LogoutButton iconOnly />}
+    />
+  )
 }
