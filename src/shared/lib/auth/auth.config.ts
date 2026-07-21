5d8e9c5fad04b3e304f13b716a6100e0e1179ebc
Latnove <r1ghtnowcp2@gmail.com>
2026-07-21 14:25:57 +0300

    refactor: separate auth.config.ts and auth.ts

--- src/shared/lib/auth/auth.config.ts
+++ src/shared/lib/auth/auth.config.ts
@@ -1,42 +1,6 @@
-import { prisma } from '@/shared/db/prisma'
-import { compare } from 'bcryptjs'
 import type { NextAuthConfig } from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
-import { credentialsSchema } from './credentialsSchema'
 
-export const authConfig = {
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const result = credentialsSchema.safeParse(credentials)
-
-        if (!result.success) return null
-        const { email, password } = result.data
-
-        const user = await prisma.user.findUnique({
-          where: { email },
-          select: {
-            id: true,
-            email: true,
-            name: true,
-            passwordHash: true,
-          },
-        })
-
-        if (!user) return null
-
-        const isPasswordValid = await compare(password, user.passwordHash)
-
-        if (!isPasswordValid) return null
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-        }
-      },
-    }),
-  ],
+export const baseAuthConfig: Omit<NextAuthConfig, 'providers'> = {
   callbacks: {
     jwt({ token, user }) {
       if (user) {
@@ -51,4 +15,4 @@ export const authConfig = {
       return session
     },
   },
-} satisfies NextAuthConfig
+}
